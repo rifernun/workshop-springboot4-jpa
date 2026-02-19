8b89a4a874b59c00ec254d274afdfb41b7288e6f
Richard Nunes <rifernun@gmail.com>
2026-02-19 09:51:16 -0300

    feat: OrderStatus enum

--- src/main/java/com/richard/course/entities/Order.java
+++ src/main/java/com/richard/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.richard.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.richard.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -21,11 +22,14 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -49,8 +53,18 @@ public class Order implements Serializable {
         return client;
     }
 
-    public void setUser(User client) {
+    public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return  OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
