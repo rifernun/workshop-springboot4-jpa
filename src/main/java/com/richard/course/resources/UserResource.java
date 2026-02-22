fd26d9f71ef7ead00075144de89ccdeac45baff4
Richard Nunes <rifernun@gmail.com>
2026-02-22 16:57:39 -0300

    feat: User update

--- src/main/java/com/richard/course/resources/UserResource.java
+++ src/main/java/com/richard/course/resources/UserResource.java
@@ -50,4 +50,9 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable long id, @RequestBody User obj) {
+        obj = service.update(id,obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
