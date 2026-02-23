e78aea54e913da3d6b9a267a8ae4e95a8ee9e2f3
Richard Nunes <rifernun@gmail.com>
2026-02-23 05:43:58 -0300

    feat: Exception Handling - findById

--- src/main/java/com/richard/course/services/UserService.java
+++ src/main/java/com/richard/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.richard.course.services;
 
 import com.richard.course.entities.User;
 import com.richard.course.repositories.UserRepository;
+import com.richard.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
